7f51629949be77c657001fd9bf1607dd9235ff32
Qi Zeng <ivan_zq@foxmail.com>
2021-12-05 02:12:16 +0000

    Topic 10 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -18,11 +18,16 @@ router.post('/',asyncHandler( async (req, res, next) => {
       return next();
     }
     if (req.query.action === 'register') {
-      await User.create(req.body);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'});
+      const Rule = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
+      if(Rule.test(req.body.password)){
+        await User.create(req.body);
+          res.status(201).json({success: true, msg: 'Successful created new user.'});
+      }else{
+          res.status(401).json({success: false, msg: 'Fail created new user.'})
+      }
     } else {
       const user = await User.findByUserName(req.body.username);
-        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+        if (!user) return res.status(401).json({success: false, msg: 'Authentication failed. User not found.' });
         user.comparePassword(req.body.password, (err, isMatch) => {
           if (isMatch && !err) {
             // if user is found and password matches, create a token
@@ -30,7 +35,7 @@ router.post('/',asyncHandler( async (req, res, next) => {
             // return the information including token as JSON
             res.status(200).json({success: true, token: 'BEARER ' + token});
           } else {
-            res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
+            res.status(401).json({success: false, msg: 'Authentication failed. Wrong password.'});
           }
         });
       }
@@ -48,21 +53,24 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-//Add a favourite. No Error Handling Yet. Can add duplicates too!
-router.post('/:userName/favourites', asyncHandler(async (req, res) => {
-    const newFavourite = req.body.id;
-    const userName = req.params.userName;
-    const movie = await movieModel.findByMovieDBId(newFavourite);
-    const user = await User.findByUserName(userName);
-    await user.favourites.push(movie._id);
-    await user.save(); 
-    res.status(201).json(user); 
-  }))
-
   router.get('/:userName/favourites', asyncHandler( async (req, res) => {
     const userName = req.params.userName;
     const user = await User.findByUserName(userName).populate('favourites');
     res.status(200).json(user.favourites);
+  }));
+
+  router.post('/:userName/favourites', asyncHandler(async (req, res) => {
+    const newFavourite = req.body.id;
+    const userName = req.params.userName;
+    const movie = await movieModel.findByMovieDBId(newFavourite);
+    const user = await User.findByUserName(userName);
+    if(!user.favourites.includes(movie._id)){
+       await user.favourites.push(movie._id);
+        await user.save(); 
+        res.status(201).json(user);  
+    }else {
+        res.status(401).json({success: false, msg: 'Error!!!'});
+      }
   }));
 
 
